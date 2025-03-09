e6f841ac2bc17f6a3b70a7dc79fb6cf5cbcb0aea
elcbil <nabilahdaa@gmail.com>
2025-03-09 23:47:10 +0700

    Menambahkan Hero

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    content: ["./src/index.html", "./src/**/*.{js,jsx,ts,tsx}"], 
+    theme: {
+      extend: {
+        fontFamily: {
+          philosopher: ["Philosopher", "sans-serif"],
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
